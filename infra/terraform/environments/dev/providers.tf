38bb3a6d6996c15d4b1a49832c34a48afe87d127
georgezefko <georgioszefkilis@gmail.com>
2026-01-14 21:11:41 +0100

    changes

--- infra/terraform/environments/dev/providers.tf
+++ infra/terraform/environments/dev/providers.tf
@@ -1,13 +1,3 @@
-variable "kubeconfig_path" {
-  type    = string
-  default = "/home/vscode/.kube/config"
-}
-
-variable "kube_context" {
-  type    = string
-  default = "kind-development"
-}
-
 provider "kubernetes" {
   config_path    = var.kubeconfig_path
   config_context = var.kube_context
